30ff3b4e9cee8e134f757c5f536f8e6bfe6cc813
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-03-06 13:47:42 -0500

    non regex validator now works!

--- validCode.php
+++ validCode.php
@@ -1,21 +1,34 @@
 <?php
 
+
+$parts = array("AP-12-3507", "   ap-99-X109   ", "SG-05-ab20", "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
+
+foreach ($parts as $part) {
+    if (validPart($part)){
+        echo "<p>$part is valid</p>";
+    } else {
+        echo "<p>$part is not valid</p>";
+    }
+}
+
+
+
+
 function validPart($str)
 {
     $str = strtolower(trim($str));
-    $category = substr($str, 0, 3);
+    $category = substr($str, 0, 2);
 
-    switch ($str) {
-        case "hw-":
-        case "sg-":
-        case "ap-":
-            break;
-        default:
-            return false;
+
+    $validCategory = array ("ap", 'hw', 'sg');
+
+    if (!in_array($category, $validCategory)){
+        return false;
+
     }
 
     $warehouse = substr($str, 3, 2);
-    if (!is_numeric($str)) {
+    if (!is_numeric($warehouse)) {
         return false;
     }
 
@@ -25,9 +38,17 @@ function validPart($str)
         return false;
     }
 
-    $partNum = substr($str, 6, 4);
+    $dash = substr($str, 2, 1);
 
-    if (!ctype_alpha($partNum)) {
+    if ($dash != '-') {
         return false;
     }
+
+    $partNum = substr($str, 6, 4);
+
+    if (!ctype_alnum($partNum) || strlen($partNum) != 4) {
+        return false;
+    }
+
+    return true;
 }
