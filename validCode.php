62c533c1a682f139ddede4529d4755506d1c02c1
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-03-06 13:32:13 -0500

    Added validPart method

--- validCode.php
+++ validCode.php
@@ -1,6 +1,33 @@
 <?php
 
-public function validPart()
+function validPart($str)
 {
+    $str = strtolower(trim($str));
+    $category = substr($str, 0, 3);
 
+    switch ($str) {
+        case "hw-":
+        case "sg-":
+        case "ap-":
+            break;
+        default:
+            return false;
+    }
+
+    $warehouse = substr($str, 3, 2);
+    if (!is_numeric($str)) {
+        return false;
+    }
+
+    $dash = substr($str, 5, 1);
+
+    if ($dash != '-') {
+        return false;
+    }
+
+    $partNum = substr($str, 6, 4);
+
+    if (!ctype_alpha($partNum)) {
+        return false;
+    }
 }
